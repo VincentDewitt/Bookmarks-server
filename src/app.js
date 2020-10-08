2298934ad8f01895f2b46fa06dba199d83efaf79
Vincent Cataldo <vincentcataldo@me.com>
2020-10-08 14:59:15 -0700

    refactored

--- src/app.js
+++ src/app.js
@@ -3,27 +3,24 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+const { NODE_ENV } = require('./config')
 const validateBearerToken = require('./validate-token')
 const errorHandler = require('./error-handler')
-const winston = require('winston')
 const bookmarksRouter = require('./bookmarks-router')
-const bookmarksService = require('./bookmarks-service')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-? 'tiny'
-: 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
+app.use(helmet())
 app.use(validateBearerToken)
-app.use('/',bookmarksRouter)
 
-app.get('/', (req,res) => {
-    res.send('Hello, world!')
+app.use('/api/bookmarks', bookmarksRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
 app.use(errorHandler)
